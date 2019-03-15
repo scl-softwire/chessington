056efe3073be0478df75fdff6fc371956b5a3f7a
Sam Cappleman-Lynes <sam.cappleman-lynes@softwire.com>
2019-03-15 14:06:11 +0000

    Allowed pawns to move forwards two squares on first move

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -17,8 +17,20 @@ public class Pawn extends AbstractPiece {
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
         List<Move> allowedMoves = new ArrayList<>();
         int offset = colour == PlayerColour.WHITE ? -1 : 1;
+
+        // Move forward one square
         allowedMoves.add(new Move(from, from.plus(offset, 0)));
 
+        // Move forward two squares
+        if (onStartingSquare(from)) {
+            allowedMoves.add(new Move(from, from.plus(2 * offset, 0)));
+        }
+
         return allowedMoves;
+    }
+
+    private boolean onStartingSquare(Coordinates currentSquare) {
+        int startingRow = colour == PlayerColour.WHITE ? 6 : 1;
+        return currentSquare.getRow() == startingRow;
     }
 }
